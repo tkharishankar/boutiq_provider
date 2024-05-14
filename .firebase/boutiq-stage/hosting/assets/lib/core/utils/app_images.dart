49265edf035cbe587c5e3575c3f8b969d542b92d
Harik <tkharishankar@gmail.com>
2024-05-14 16:50:46 +0400

    firebase hosting config changed

--- .firebase/boutiq-stage/hosting/assets/lib/core/utils/app_images.dart
+++
@@ -1,7 +0,0 @@
-class AppImages {
-  static const _baseAssetUrl = 'assets/images/';
-
-  static String logo = '${_baseAssetUrl}logo.png';
-
-  static String loader = '${_baseAssetUrl}loading.png';
-}
--- .firebase/boutiq-stage/hosting/assets/lib/features/auth/presentation/login/states/login_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'login_bloc.dart';
-
-@freezed
-class LoginState with _$LoginState {
-  const factory LoginState.initial() = _Initial;
-  const factory LoginState.loading() = _Loading;
-  const factory LoginState.loginError(String message) = _LoginError;
-  const factory LoginState.loginSuccessful(String message) = _LoginSuccessful;
-}
